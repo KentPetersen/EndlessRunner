dfeef110ad511d01cc1717e27529b81bbe6a0558
KentPetersen <KentPetersen@users.noreply.github.com>
2017-10-17 10:53:57 -0700

    update 2 with score bug and prefab bug
    added equipment buttons gravity / dbl jump to pause menu
    added currency count to main screen and saving
    added level count to main screen
    level count increases when a certain score is received
    
    scoring bug score doesnt display errors
    prefab bug player gets stuck on nothing

---
+++ Runner/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+	public ScoreManager theScoreManager;
+
+	public Text levelText;
+	public int levelCount;
+	public int levelIncreaseMilestone;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//level up every X points
+		if (theScoreManager.scoreCount > levelIncreaseMilestone * levelCount) {
+			levelCount++;
+		}
+
+		levelText.text = "Level: " + levelCount;
+		
+	}
+}
